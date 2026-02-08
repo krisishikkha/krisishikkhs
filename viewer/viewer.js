0125ace07e583e875207eed45b988b5d4d3d2cf0
krisishikkha <krisishikkha@gmail.com>
2026-02-08 22:38:20 +0600

    Update viewer.js

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -1,24 +1,40 @@
 const params = new URLSearchParams(window.location.search);
 const pdfUrl = params.get("pdf");
 
+if (!pdfUrl) {
+  alert("PDF path not found");
+}
+
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-"/krisishikkha/vendor/pdfjs/pdf.worker.min.js";
+"../vendor/pdfjs/pdf.worker.min.js";
 
 const container = document.getElementById("pdf-container");
 
 pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
-  for (let p = 1; p <= pdf.numPages; p++) {
-    pdf.getPage(p).then(page => {
-      const viewport = page.getViewport({ scale: 1.3 });
+
+  for (let i = 1; i <= pdf.numPages; i++) {
+    pdf.getPage(i).then(page => {
+
+      const scale = 1.25;
+      const viewport = page.getViewport({ scale });
+
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d");
 
       canvas.width = viewport.width;
       canvas.height = viewport.height;
-      canvas.style.marginBottom = "20px";
+      canvas.style.display = "block";
+      canvas.style.margin = "0 auto 20px";
 
       container.appendChild(canvas);
-      page.render({ canvasContext: ctx, viewport });
+
+      page.render({
+        canvasContext: ctx,
+        viewport: viewport
+      });
     });
   }
+
+}).catch(err => {
+  alert("PDF load error: " + err.message);
 });
