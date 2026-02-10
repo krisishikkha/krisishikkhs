790e28c54f20c0c9435391e3a85acb591b815994
krisishikkha <krisishikkha@gmail.com>
2026-02-10 23:56:38 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,37 @@
 const CACHE_NAME = "krisishikkha-v1";
 
-const ASSETS_TO_CACHE = [
+const FILES_TO_CACHE = [
   "/krisishikkha/",
   "/krisishikkha/index.html",
-  "/krisishikkha/ntrca.html",
   "/krisishikkha/manifest.json",
-
-  "/krisishikkha/assets/css/style.css",
-
   "/krisishikkha/assets/icons/icon-192.png",
-  "/krisishikkha/assets/icons/icon-512.png",
-
-  "/krisishikkha/vendor/pdfjs/pdf.min.js",
-  "/krisishikkha/vendor/pdfjs/pdf.worker.min.js",
-
-  "/krisishikkha/viewer/index.html",
-  "/krisishikkha/viewer/viewer.js",
-
-  "/krisishikkha/pages/categories/bcs.html",
-  "/krisishikkha/pages/categories/research.html",
-  "/krisishikkha/pages/categories/others.html"
+  "/krisishikkha/assets/icons/icon-512.png"
 ];
 
-/* 🔹 INSTALL */
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-/* 🔹 ACTIVATE */
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
   self.clients.claim();
 });
 
-/* 🔹 FETCH */
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
