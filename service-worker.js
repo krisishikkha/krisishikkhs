89ad1ed8a42ec67935fa3fcc0de302393b98e1e5
krisishikkha <krisishikkha@gmail.com>
2026-02-11 00:07:34 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-const CACHE_NAME = "krisishikkha-v1";
+const CACHE_NAME = 'krisishikkha-v1';
 
-const FILES_TO_CACHE = [
-  "/krisishikkha/",
-  "/krisishikkha/index.html",
-  "/krisishikkha/manifest.json",
-  "/krisishikkha/assets/icons/icon-192.png",
-  "/krisishikkha/assets/icons/icon-512.png"
+const OFFLINE_URLS = [
+  '/krisishikkha/',
+  '/krisishikkha/index.html',
+  '/krisishikkha/manifest.json',
+  '/krisishikkha/ntrca.html',
+  '/krisishikkha/privacy-policy.html',
+  '/krisishikkha/terms.html',
+  '/krisishikkha/assets/icons/icon-192.png',
+  '/krisishikkha/assets/icons/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
@@ -32,7 +33,9 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
