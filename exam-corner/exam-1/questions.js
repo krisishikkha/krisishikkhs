9ec2fa7011823997c9e6e055b79fb91fa398f93b
krisishikkha <krisishikkha@gmail.com>
2026-02-15 00:27:44 +0600

    Create questions.js

---
+++ exam-corner/exam-1/questions.js
@@ -0,0 +1,13 @@
+const QUESTIONS = [
+  {
+    question: "ধানের বৈজ্ঞানিক নাম কী?",
+    options: [
+      "Oryza sativa",
+      "Triticum aestivum",
+      "Zea mays",
+      "Hordeum vulgare"
+    ],
+    answer: 0,
+    explanation: "ধানের বৈজ্ঞানিক নাম Oryza sativa।"
+  }
+];
