f281c265c727f2c6d2f90e7bb30f55fbb78c2d9a
krisishikkha <krisishikkha@gmail.com>
2026-02-14 23:34:54 +0600

    Create exam-status.js

---
+++ exam-status.js
@@ -0,0 +1,5 @@
+const EXAM_STATUS = {
+  "exam-1": { title: "টেস্ট পরীক্ষা ১", status: "live" },
+  "exam-2": { title: "টেস্ট পরীক্ষা ২", status: "locked" },
+  "exam-3": { title: "টেস্ট পরীক্ষা ৩", status: "locked" }
+};
