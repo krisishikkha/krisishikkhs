4589259e354d34b253337ec31bf3356f5cae7449
krisishikkha <krisishikkha@gmail.com>
2026-02-18 22:13:14 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -1,5 +1,17 @@
 const EXAM_STATUS = {
-  "exam-1": { title: "টেস্ট পরীক্ষা ১", status: "live" },
-  "exam-2": { title: "টেস্ট পরীক্ষা ২", status: "locked" },
-  "exam-3": { title: "টেস্ট পরীক্ষা ৩", status: "locked" }
+  "exam-1": { 
+      title: "ডেমো পরীক্ষা ১", 
+      status: "live",
+      codes: ["krisi"]
+  },
+  "exam-2": { 
+      title: "ডেমো পরীক্ষা ২", 
+      status: "locked",
+      codes: ["exam2code"]
+  },
+  "exam-3": { 
+      title: "ডেমো পরীক্ষা ৩", 
+      status: "locked",
+      codes: ["exam3code"]
+  }
 };
