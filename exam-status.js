75c546558574f205f260edd28b76909cd6532fa3
krisishikkha <krisishikkha@gmail.com>
2026-02-21 16:06:29 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -1,17 +1,22 @@
 const EXAM_STATUS = {
-  "exam-1": { 
-      title: "ডেমো পরীক্ষা ১ 🔴", 
-      status: "live",
-      codes: ["krisi"]
+  "exam-1": {
+    title: "ডেমো পরীক্ষা 🔴",
+    visible: true,   // ✅ নতুন
+    status: "live",
+    codes: ["krisi"]
   },
-  "exam-2": { 
-      title: "HSC 1st Paper 1st Chapter", 
-      status: "locked",
-      codes: ["exam2code"]
+
+  "exam-2": {
+    title: "HSC 1st Paper 1st Chapter",
+    visible: true,  // ✅ নতুন
+    status: "locked",
+    codes: ["exam2code"]
   },
-  "exam-3": { 
-      title: "HSC 1st Paper 2nd Chapter", 
-      status: "locked",
-      codes: ["exam3code"]
+
+  "exam-3": {
+    title: "HSC 1st Paper 2nd Chapter",
+    visible: false,  // ✅ নতুন
+    status: "locked",
+    codes: ["exam3code"]
   }
 };
